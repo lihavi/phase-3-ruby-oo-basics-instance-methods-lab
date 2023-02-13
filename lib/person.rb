fe7bea8db7f7540ce5e62b6739f93d808de5f72a
vivianne <viviannjoroge91@gmail.com>
2023-02-13 18:40:17 +0300

    commit tree f9f48ea6cccb331674ba41cf3f0234fdd391bf09

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+Talitha = Person.new
+Talitha.talk
+
+Val = Person.new
+Val.walk
